ea7a23396d4247d1aea55704078a4b9c00a6c1e5
Alex Saber <Alex Saber>
2016-07-30 16:44:15 -0500

    declared stackLetters(); finalized part 1 of the exercise

--- stackLetters.js
+++ stackLetters.js
@@ -1,23 +1,10 @@
-/*
-Create a stackLetters function that should console.log rows of letters.
-It should take an array containing the letters of the alphabet and output them like this:
-
-a
-ab
-abc
-abcd
-abcde
-abcdef
-etc
-
-Then add logic to stackLetters that places a space after every fifth letter, like this:
-
-a
-ab
-abd
-abcd
-abcde
-abcde f
-etc
-*/
-
+var letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+function stackLetters() {
+  var letterRow = "";
+  var noSpaces = "";
+  for (var i = 0; i < letters.length; i++) {
+    letterRow += letters[i];
+    console.log(letterRow);
+  }
+}
+stackLetters();
